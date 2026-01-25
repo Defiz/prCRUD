2a26b88196baf09054c7cf1bba6802ef91afc737
pc <vladk274@gmail.com>
2026-01-25 20:32:08 +0300

    delete AuthProvider

--- src/main/java/vlad/pr/projectCRUD/config/SecurityConfig.java
+++ src/main/java/vlad/pr/projectCRUD/config/SecurityConfig.java
@@ -1,19 +1,27 @@
 package vlad.pr.projectCRUD.config;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import vlad.pr.projectCRUD.security.AuthProviderImpl;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import vlad.pr.projectCRUD.service.UsersDetailsService;
 
 @AllArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfiguration {
 
-    private final AuthProviderImpl authProvider;
+    private final UsersDetailsService usersDetailsService;
 
     // настройка аутентификации
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(usersDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
